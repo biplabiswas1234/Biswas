40221a6194eec0cc32040f453887be3dce1ce91f
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-13 11:22:12 +0530

    Uc1 Bip

---
+++ Demmoo2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demmoo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FlipCoin obj = new FlipCoin();
+            obj.Flip_Coin();
+        }
+    }
+}
